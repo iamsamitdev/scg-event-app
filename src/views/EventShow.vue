87bd768786487e9c1424801b604c749ab68bb921
Samit Koyom <samitkoyom@gmail.com>
2019-07-24 16:51:11 +0700

    fist commit

---
+++ src/views/EventShow.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Event list #{{ id }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            id:{
+                required: true
+            }
+        }
+    }
+</script>
